18cae26c0b7aebcd868f32ed6bbda3387f8f3537
Victor Cardoso <victor_92santos@gmail.com>
2019-10-27 19:49:02 -0300

    teste de falha email ja existe

--- __tests__/sing-up.test.js
+++ __tests__/sing-up.test.js
@@ -3,26 +3,39 @@ import app from '../src/app';
 
 import User from '../src/app/models/User';
 
-beforeEach(() => {
-  User.collection.drop();
-});
+const body = {
+  nome: 'victor',
+  email: 'v2@gmail.com',
+  senha: '12345678',
+  telefones: [
+    {
+      ddd: '11',
+      numero: '972820922',
+    },
+  ],
+};
 
-describe('user', () => {
+describe('User sucesso', () => {
+  beforeEach(() => {
+    User.collection.drop();
+  });
+
   it('deve ser possivel cadastrar um usuario e obter um token', async () => {
     const response = await request(app)
       .post('/sing-up')
-      .send({
-        nome: 'victor',
-        email: 'v2@gmail.com',
-        senha: '12345678',
-        telefones: [
-          {
-            ddd: '11',
-            numero: '972820922',
-          },
-        ],
-      });
+      .send(body);
 
     expect(response.body).toHaveProperty('_id');
+  });
+});
+
+describe('User falha', () => {
+  it('Deve retornar que o email ja existe', async () => {
+    const response = await request(app)
+      .post('/sing-up')
+      .send(body)
+      .expect(400);
+
+    expect(response.body).toHaveProperty('mensagem');
   });
 });
