cdcffaef0176fbdc7050fa3e207081322c4c79b2
Victor Cardoso <victor_92santos@gmail.com>
2019-10-27 19:55:25 -0300

    Criptografando senha do usuario

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,6 @@
+/* eslint-disable func-names */
 import { Schema, model } from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const UserSchema = new Schema(
   {
@@ -25,5 +27,12 @@ const UserSchema = new Schema(
     versionKey: false,
   }
 );
+
+UserSchema.pre('save', async function(next) {
+  if (this.senha) {
+    this.senha = await bcrypt.hash(this.senha, 8);
+  }
+  next();
+});
 
 export default model('User', UserSchema);
